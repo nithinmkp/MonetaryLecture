4e5df5d463765a630f1eba2d06ab319e4b091736
Nithin M <write2nithinm@gmail.com>
2021-03-09 21:21:22 +0530

    Made date to truncate softcoded

--- code_differential.R
+++ code_differential.R
@@ -11,7 +11,8 @@ data2<-list.cbind(data) %>% select(1,2,4,6)
 
 #forward rates
 data3<-data2 %>% mutate(forward_rate=lead(`Spot Rate`,n=12))
-data3<-data3 %>% filter(Date<=as.Date("2019-12-01"))
+
+data3<-data3 %>% filter(Date<as.Date(data3[which(is.na(data3$forward_rate))[1],1]))
 data3<-data3 %>% 
         mutate(int_diff=((1+data3$`Domestic Rate`/100)-(1+data3$`Foreign Rate`/100))*data3$forward_rate/data3$`Spot Rate`)
 
@@ -33,3 +34,4 @@ r<-q %>% add_trace(x=~Date,y=~int_diff,type="scatter",mode="lines",
                xaxis=list(type="date",
                           tickformat="%b-%Y",title="Date"),
                yaxis=list(title="differnital (in %)"))
+r
